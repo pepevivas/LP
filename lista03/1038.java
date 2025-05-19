cae60db05a433e5dafa195a336a22dc84e7b5ab5
Pedro Vidal <pedrovidalsilva13@gmail.com>
2025-05-19 11:13:25 -0300

    oi

---
+++ lista03/1038.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+class Lanche{
+    int codigo;
+    String espec = new String();
+    double preco;
+
+}
+class Cardapio{
+    Lanche[] itens;
+}
+class Pedido{
+    int qte;
+    Lanche lanche;
+    double precofinal;
+}
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+    }
+ 
+}
